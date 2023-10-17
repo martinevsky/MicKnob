82084739e0853a1bd87ec517456952eaddff89c5
Max Martinevsky <max@martinevsky.ru>
2023-10-17 23:00:23 +0300

    First working version

--- main.cpp
+++ main.cpp
@@ -1,3 +1,56 @@
+#include "mic.h"
+#include "knob.h"
+
+auto Swallow (auto fun)
+{
+    return [fun = std::move (fun)]() noexcept
+        {
+            try
+            {
+                fun();
+            }
+            catch(...)
+            {
+                // TODO: Trace something
+            }
+        };
+}
+
 int main()
 {
+    try
+    {
+        Mic mic;
+        Knob knob;
+
+        knob.SetPushHandler (Swallow(
+            [&]()
+            {
+                mic.ToggleMute();
+            }
+        ));
+
+        knob.SetSpinLeftHandler (Swallow(
+            [&]()
+            {
+                mic.DecVol();
+            }
+        ));
+
+        knob.SetSpinRightHandler (Swallow(
+            [&]()
+            {
+                mic.IncVol();
+            }
+        ));
+
+        knob.Loop();
+
+        return EXIT_SUCCESS; 
+    }
+    catch (...)
+    {
+        // TODO: Trace something
+        return EXIT_FAILURE;
+    }
 }
